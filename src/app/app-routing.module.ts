5ff9f8c1abdb7b257f836a4ef1e413beba59c6fc
Felipe Salamanca <juanf.salamancag@konradlorenz.edu.co>
2024-10-16 17:53:30 -0500

    Configuración Inicial mas consulta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { AnimalComponent } from './components/animal/animal.component';
+const routes: Routes = [
+  {path: 'animal', component: AnimalComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AnimalComponent } from './components/animal/animal.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnimalComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+  BrowserModule,
+  AppRoutingModule,
+  BrowserAnimationsModule,
+  FormsModule,
+  HttpClientModule,
+  CommonModule,
+  ReactiveFormsModule 
   ],
   providers: [
     provideClientHydration()
